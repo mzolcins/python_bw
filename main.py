3d11abaec2a2cec632a7e2c9e685803b6bbe4a27
Walchuck997 <142448238+Walchuck997@users.noreply.github.com>
2023-08-16 22:22:52 +0200

    pierwszy kod
    pumba chuj

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Walczak')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def print_jebacpis(name):
+    print(f'Jebać {name}')
+
+name=input()
+print_jebacpis(name)
+name = True
+print_jebacpis(name)
